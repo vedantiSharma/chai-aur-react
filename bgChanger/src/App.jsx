af126aaa5a93762c6d87c2c66dfa95d766418e0f
vedantiSharma <vedantis2812btechcse2024@kccitm.edu.in>
2023-12-18 20:40:42 +0530

    not completed the DIV

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("black")
+
+ 
+  return (
+<div className="m-w-full h-screen duration-200" style={{ backgroundColor: color }}>
+  fdfdfdf
+
+      <div  className=' fixed flex flex-wrap  justify center bottom-12 inset-x-50 px-5'> 
+      <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2"> style
+        </div>
+        </div>
+    </div>
+    
+
+  )
+}
+
+export default App
