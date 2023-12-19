72fb23876d3712e80ed2d6707b938582c7d36e14
vedantiSharma <vedantis2812btechcse2024@kccitm.edu.in>
2023-12-19 11:43:19 +0530

    bg-changing onclick tailwindcss CODE

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -4,18 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("black")
+  const [color, setColor] = useState("olive")
 
  
   return (
-<div className="m-w-full h-screen duration-200" style={{ backgroundColor: color }}>
-  fdfdfdf
+<div  className='w-full  h-screen duration-200' style={{background: color}}>
+  
 
-      <div  className=' fixed flex flex-wrap  justify center bottom-12 inset-x-50 px-5'> 
-      <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2"> style
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-52 px-5' >
+      <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2"> 
+
+      <button onClick={()=> setColor("green")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"green"} } >Green</button>
+      <button onClick={()=> setColor("yellow")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"yellow"} } >Yellow</button>
+
+      <button onClick={()=> setColor("pink")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"pink"} } >pink</button>
+
+      <button onClick={()=> setColor("orange")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"orange"} } >orange</button>
+      <button onClick={()=> setColor("blue")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"blue"} } >Blue</button>
+      <button onClick={()=> setColor("purple")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"purple"} } >purple</button>
+      <button onClick={()=> setColor("grey")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"grey"} } >grey</button>
+      <button onClick={()=> setColor("red")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"red"} } >Red</button>
+      <button onClick={()=> setColor("black")} className=' outline-none px-4 py-1 rounded-full ' style={{background:"black"} } >Black</button>
+
+
         </div>
         </div>
-    </div>
+     </div>
     
 
   )
