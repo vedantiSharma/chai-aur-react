defa9047b9cced8b6b2a36fde0971d14349170bc
vedantiSharma <vedantis2812btechcse2024@kccitm.edu.in>
2024-03-02 15:24:46 +0530

    .env files only

---
+++ megaProject/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+console.log(import.meta.env.VITE_APPWRITE_URL );
+  return (
+    
+      <>
+      <h1>appplication run </h1></>
+      
+  )
+}
+
+export default App
