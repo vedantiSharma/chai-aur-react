dc4c86f5c7f5c2060cf5256d897eda237bce35d7
Shivam Sharma <shivam111sharma@gmail.com>
2024-03-14 15:14:40 +0530

    authntication-completed

--- megaProject/src/appwrite/auth.js
+++ megaProject/src/appwrite/auth.js
@@ -1,43 +1,72 @@
-import config from '../config/config'
-import {ID , Client , Account} from "appwrite"
+import {config} from "../config/config"
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client //giving refrence of client variavble
+            .setEndPoint(appwriteUrl)
+            .setProject(appwriteProjectId);
 
-export class AuthService{
-client = new Client();
-account;
-constructor(){
-    this.client //giving refrence of client variavble
-        .setEndPoint(appwrite_url)
-        .setProject(appwrite_project_id);
-
-        this.account = new Account();
-}
-
-async createAccount({email , password, name}) //makes understand that we pss the value and account will created
-//use try-catch if in case account not created or created or half credentials etc
-{
-    try {
-        const userAccount = await this.account.create(ID.unique() , email , password , name);
-            if(userAccount){
-                return userAccount
-            }
-            else{
-                
-            }
-
-
-
-    } catch (error) {
-        throw error;
+        this.account = new Account(this.client);
     }
+
+    async createAccount({ email, password, name }) //makes understand that we pss the value and account will created //either use promise or async
+    //use try-catch if in case account not created or created or half credentials etc
+    {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email , password})
+            }
+            else {
+                return userAccount;
+
+            }
+
+
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({}){ //method login
+        try {
+              return  await this.account.createEmailSession(email , password)   
+            
+        } catch (error) {
+            throw error ;
+            
+        }
+    }
+
+    async GetCurrentUser(){
+        try {
+          return await this.account.get({})
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+
+
+
 }
 
 
 
-}
-
-
-
-    const authservice = new AuthService();//creation of obj authservice
+const authservice = new AuthService();//creation of obj authservice to call the class
 
 
 
--- megaProject/src/config/config.js
+++ megaProject/src/config/config.js
@@ -1,9 +1,9 @@
 const config ={
-    appwrite_url : String(import.meta.env.VITE_APPWRITE_URL),
-    appwrite_project_id : String(import.meta.env.VITE_PROJECT_ID),
-    appwrite_database_id : String(import.meta.env.VITE_DATABASE_ID),
-    appwrite_collection_id : String(import.meta.env.VITE_COLLECTION_ID),
-    appwrite_bucket_id: String(import.meta.env.VITE_BUCKET_ID),
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
 
 
 
